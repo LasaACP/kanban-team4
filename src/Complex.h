8406d35f93b277fb2c2ad45bea828ecae28e19c7
Alexander Burton <amacburton@gmail.com>
2016-11-07 11:20:04 -0600

    started adding operators

--- src/Complex.h
+++ src/Complex.h
@@ -5,10 +5,21 @@ class Complex {
 	private:
 		double real, imaginary;
 	public:
-		Complex();
-		Complex(int);
-		Complex(float);
-		Complex(double);
+		// Basic assignment operators
+		// Takes different types of numbers
+		void operator=(int);
+		void operator=(long int);
+		void operator=(float);
+		void operator=(double);
+		void operator=(const Complex&);
+		
+		// Basic arithmetic operators
+		// Can use different types of numbers
+		Complex operator+(int);
+		Complex operator+(long int);
+		Complex operator+(float);
+		Complex operator+(double);
+		Complex operator+(const Complex&);
 }
 
 #endif
