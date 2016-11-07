38520127c7264521ffa1feb00c2ff2c94a3d1203
Alexander Burton <amacburton@gmail.com>
2016-11-07 11:07:47 -0600

    added main header file

---
+++ src/Complex.h
@@ -0,0 +1,14 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+class Complex {
+	private:
+		double real, imaginary;
+	public:
+		Complex();
+		Complex(int);
+		Complex(float);
+		Complex(double);
+}
+
+#endif
