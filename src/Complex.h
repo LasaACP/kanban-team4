eee29406879c63e9a4b44d56581a00b7c22758ed
Alexander Burton <amacburton@gmail.com>
2016-11-08 11:12:16 -0600

    added prototypes for operators

--- src/Complex.h
+++ src/Complex.h
@@ -5,21 +5,212 @@ class Complex {
 	private:
 		double real, imaginary;
 	public:
-		// Basic assignment operators
-		// Takes different types of numbers
-		void operator=(int);
-		void operator=(long int);
-		void operator=(float);
-		void operator=(double);
-		void operator=(const Complex&);
+	/* OPERATORS */
+	
+		/* Basic assignment operators */
+
+			void operator=(int);
+			void operator=(long int);
+			void operator=(float);
+			void operator=(double);
+			void operator=(const Complex&);
+
+		/* Integer promotion and negation */
+
+			Complex operator+();
+			Complex operator-();
 		
-		// Basic arithmetic operators
-		// Can use different types of numbers
-		Complex operator+(int);
-		Complex operator+(long int);
-		Complex operator+(float);
-		Complex operator+(double);
-		Complex operator+(const Complex&);
+		/* Basic arithmetic operators */
+
+			Complex operator+(int);
+			Complex operator+(long int);
+			Complex operator+(float);
+			Complex operator+(double);
+			Complex operator+(const Complex&);
+
+			Complex operator-(int);
+			Complex operator-(long int);
+			Complex operator-(float);
+			Complex operator-(double);
+			Complex operator-(const Complex&);
+			
+			Complex operator*(int);
+			Complex operator*(long int);
+			Complex operator*(float);
+			Complex operator*(double);
+			Complex operator*(const Complex&);
+			
+			Complex operator/(int);
+			Complex operator/(long int);
+			Complex operator/(float);
+			Complex operator/(double);
+			Complex operator/(const Complex&);
+			
+			Complex operator%(int);
+			Complex operator%(long int);
+			Complex operator%(float);
+			Complex operator%(double);
+			Complex operator%(const Complex&);
+
+		/* Increment/decrement */
+
+			// prefix
+			Complex& operator++();
+			// postfix 
+			Complex operator++(int);
+
+			// prefix
+			Complex& operator--();
+			// postfix
+			Complex operator--(int);
+
+		/* Comparision */
+
+			bool operator==(int);
+			bool operator==(long int);
+			bool operator==(float);
+			bool operator==(double);
+			bool operator==(Complex&);
+
+			bool operator!=(int);
+			bool operator!=(long int);
+			bool operator!=(float);
+			bool operator!=(double);
+			bool operator!=(Complex&);
+
+			bool operator>(int);
+			bool operator>(long int);
+			bool operator>(float);
+			bool operator>(double);
+			bool operator>(Complex&);
+
+			bool operator<(int);
+			bool operator<(long int);
+			bool operator<(float);
+			bool operator<(double);
+			bool operator<(Complex&);
+
+			bool operator>=(int);
+			bool operator>=(long int);
+			bool operator>=(float);
+			bool operator>=(double);
+			bool operator>=(Complex&);
+
+			bool operator<=(int);
+			bool operator<=(long int);
+			bool operator<=(float);
+			bool operator<=(double);
+			bool operator<=(Complex&);
+
+		/* Logical */
+
+			// N/A
+
+		/* Bitwise */
+
+			Complex operator~();
+
+			Complex operator&(int);
+			Complex operator&(long int);
+			Complex operator&(float);
+			Complex operator&(double);
+			Complex operator&(Complex);
+
+			Complex operator|(int);
+			Complex operator|(long int);
+			Complex operator|(float);
+			Complex operator|(double);
+			Complex operator|(Complex);
+
+			Complex operator^(int);
+			Complex operator^(long int);
+			Complex operator^(float);
+			Complex operator^(double);
+			Complex operator^(Complex);
+
+			Complex operator<<(int);
+			Complex operator<<(long int);
+			Complex operator<<(float);
+			Complex operator<<(double);
+			Complex operator<<(Complex);
+
+			Complex operator>>(int);
+			Complex operator>>(long int);
+			Complex operator>>(float);
+			Complex operator>>(double);
+			Complex operator>>(Complex);
+
+		/* Compound assignment */
+
+			Complex operator+=(int);
+			Complex operator+=(long int);
+			Complex operator+=(float);
+			Complex operator+=(double);
+			Complex operator+=(const Complex&);
+
+			Complex operator-=(int);
+			Complex operator-=(long int);
+			Complex operator-=(float);
+			Complex operator-=(double);
+			Complex operator-=(const Complex&);
+			
+			Complex operator*=(int);
+			Complex operator*=(long int);
+			Complex operator*=(float);
+			Complex operator*=(double);
+			Complex operator*=(const Complex&);
+			
+			Complex operator/=(int);
+			Complex operator/=(long int);
+			Complex operator/=(float);
+			Complex operator/=(double);
+			Complex operator/=(const Complex&);
+			
+			Complex operator%=(int);
+			Complex operator%=(long int);
+			Complex operator%=(float);
+			Complex operator%=(double);
+			Complex operator%=(const Complex&);
+
+			Complex operator|=(int);
+			Complex operator|=(long int);
+			Complex operator|=(float);
+			Complex operator|=(double);
+			Complex operator|=(Complex);
+
+			Complex operator^=(int);
+			Complex operator^=(long int);
+			Complex operator^=(float);
+			Complex operator^=(double);
+			Complex operator^=(Complex);
+
+			Complex operator<<=(int);
+			Complex operator<<=(long int);
+			Complex operator<<=(float);
+			Complex operator<<=(double);
+			Complex operator<<=(Complex);
+
+			Complex operator>>=(int);
+			Complex operator>>=(long int);
+			Complex operator>>=(float);
+			Complex operator>>=(double);
+			Complex operator>>=(Complex);
+
+		/* Pointer */
+			
+			Complex& operator[](int);
+			Complex& operator[](long int);
+			Complex& operator[](float);
+			Complex& operator[](double);
+			Complex& operator[](Complex);
+
+		/* Other */
+
+			// Complex operator""_c(std::string);
+			void* operator new (size_t);
+			void* operator new[] (size_t);
+			void operator delete(void*);
+			void operator delete[](void*);
 }
 
 #endif
