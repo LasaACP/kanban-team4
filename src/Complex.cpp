b980638b9624f6d26952fd774e4890db6daf0ff4
Alexander Burton <amacburton@gmail.com>
2016-11-14 10:36:31 -0600

    renamed variables according to header

--- src/Complex.cpp
+++ src/Complex.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
 #include <Complex.h>
-
-using namespace std;
 
 class Complex
 {
 	private:
-		double a;
-		double b;
+		double real;
+		double imag;
 
 	public:
-		Complex(){//default constructor
-			a = 0;
-			b = 0;
+		Complex() {//default constructor
+			real = 0;
+			imag = 0;
 		}
-		Complex(double A, double B){//constructor
+		Complex(double A, double B) {//constructor
 			a = A;
 			b = B;
 		}
-		Complex(string str){//constructor
+		Complex(std::string str) {//constructor
 			for (int i =0; i < str.length(); i++){
 				if (str.at(i) == ' '){
 					str.erase(i,1);
 					i--;
 				}
 			}
-			if (str.find("i") > str.find("+")){ //a+bi
+			if (str.find("i") > str.find("+")) { //a+bi
 				a = atof(str.substr(0, str.find("+")).c_str());
 				b = atof(str.substr(str.find("+")+1,str.length()-str.find("+")+1).c_str());
 			}
-			else if (str.find("i", 0) < str.find("+", 0)){ //bi+a
+			else if (str.find("i", 0) < str.find("+", 0)) { //bi+a
 				b = atof(str.substr(0, str.find("+")).c_str());
 				a = atof(str.substr(str.find("+")+1,str.length()-str.find("+")+1).c_str());
 			}
 			else{
-				cout << "Error 1\n";
+				std::cout << "Error 1\n";
 			}
 
 		}
