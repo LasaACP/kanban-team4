16b87d9eccd0730a1445057aac0f66ded852f1ad
rmatouschekh <rhicke@gmail.com>
2016-11-16 10:33:54 -0600

    Update Complex.h

--- src/Complex.h
+++ src/Complex.h
@@ -54,32 +54,11 @@ class Complex {
 			Complex operator+(double);
 			Complex operator+(const Complex&);
 
-			Complex operator-(int a)
-			{
-				this.setReal(this.getReal()-double(a));
-				return this;
-			}
-			Complex operator-(long int a)
-			{
-				this.setReal(this.getReal()-double(a));
-				return this;
-			}
-			Complex operator-(float a)
-			{
-				this.setReal(this.getReal()-double(a));
-				return this;
-			}
-			Complex operator-(double a)
-			{
-				this.setReal(this.getReal()-a);
-				return this;
-			}
-			Complex operator-(Complex& a)
-			{
-				this.setImag(this.getImag() - a.getImag());
-				this.setReal(this.getReal() - a.getReal());
-				return this;
-			}
+			Complex operator-(int);
+			Complex operator-(long int);
+			Complex operator-(float);
+			Complex operator-(double);
+			Complex operator-(Complex&);
 			
 			Complex operator*(int);
 			Complex operator*(long int);
